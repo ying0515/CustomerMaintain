9ec5483c6f00af9299b280622e57ab33c9982e8c
ying0515 <ying515.huang@msa.hinet.net>
2015-07-12 21:09:25 +0800

    加入檢視表

--- CustomerMaintain/Models/Main/MainModel.Context.cs
+++ CustomerMaintain/Models/Main/MainModel.Context.cs
@@ -28,5 +28,6 @@ namespace CustomerMaintain.Models.Main
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+        public virtual DbSet<客戶資料狀況> 客戶資料狀況 { get; set; }
     }
 }
